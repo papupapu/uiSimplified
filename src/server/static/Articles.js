467148197a1ca3f5774afbcf8792cc12491db4a1
Giancarlo Gerbaz <ganesa_2001@yahoo.it>
2017-03-12 14:21:12 +0100

    Let us use some data that actually makes sense + CSS text overflow managment for webkit browsers

--- src/server/static/Articles.js
+++ src/server/static/Articles.js
@@ -1,8 +1,8 @@
 export const articleList = [
   {
     heading: {
-      title: 'Femminicidio, via libera dalla Camera a legge per tutelare gli orfani',
-      subtitle: 'Rischia l\'ergastolo chi uccide la coniuge o la convivente. E ai figli delle vittime viene assicurato patrocinio legale e reversibilità della pensione',
+      title: 'Why Namibia Represents What Surf Travel Should Be About',
+      subtitle: 'Back when The Endless Summer came out, surf travel wasn\'t really a thing. Not in the way it is now, at least. Then Bruce Brown\'s opus was released to the masses, and the trickle of surfers curious to seek, to explore, to find, slowly became a flood. Now, of course, you\'re hard pressed to find a lineup that hasn\'t been explored. Oh, there are places, to be sure, but they\'re fewer and farther between with each passing day.',
       media: [
         {
           type: 'photo',
@@ -45,204 +45,84 @@ export const articleList = [
   },
   {
     heading: {
-      title: 'Femminicidio, via libera dalla Camera a legge per tutelare gli orfani',
-      subtitle: 'Rischia l\'ergastolo chi uccide la coniuge o la convivente. E ai figli delle vittime viene assicurato patrocinio legale e reversibilità della pensione',
+      title: 'Into Thin Air: A Personal Account of the Mt. Everest Disaster',
+      subtitle: 'In the book, Jon Krakauer described the events leading up to his eventual decision to participate in an Everest expedition in May 1996, despite having mostly given up mountain climbing years before. The 1996 season expedition recorded 8 deaths, the third most on Everest in a single day (the April 2015 Nepal earthquake caused the most, at least 19 deaths), including Krakauer\'s guides Rob Hall and Andy Harris',
       media: [
         {
           type: 'photo',
-          src: '/images/surf1.jpg',
+          src: '/images/everest1.jpg',
+        },
+        {
+          type: 'photo',
+          src: '/images/everest2.jpg',
+        },
+        {
+          type: 'photo',
+          src: '/images/everest3.jpg',
+        },
+        {
+          type: 'photo',
+          src: '/images/everest4.jpg',
+        },
+        {
+          type: 'photo',
+          src: '/images/everest5.jpg',
+        },
+        {
+          type: 'photo',
+          src: '/images/everest6.jpg',
         },
       ],
     },
   },
   {
     heading: {
-      title: 'Femminicidio, via libera dalla Camera a legge per tutelare gli orfani',
-      subtitle: 'Rischia l\'ergastolo chi uccide la coniuge o la convivente. E ai figli delle vittime viene assicurato patrocinio legale e reversibilità della pensione',
+      title: 'Why The New York Knicks Suck: it\'s 2016 not 2006',
+      subtitle: 'Nobody really expected much out of the New York Knicks this season. Why would they? The team tanked its way to 17 wins last season in a bid to rival the Washington Generals and couldn\'t even do that right. What was supposed to be an awful season en route to having the best chance at getting the #1 pick turned into a terrible season inexplicably improved by meaningless wins at the end that dropped us to the #4 slot.',
       media: [
         {
           type: 'photo',
-          src: '/images/surf1.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf2.jpg',
+          src: '/images/knicks.jpg',
         },
       ],
     },
   },
   {
     heading: {
-      title: 'Femminicidio, via libera dalla Camera a legge per tutelare gli orfani',
-      subtitle: 'Rischia l\'ergastolo chi uccide la coniuge o la convivente. E ai figli delle vittime viene assicurato patrocinio legale e reversibilità della pensione',
+      title: 'Moebius: l\'arte della fantascienza a fumetti',
+      subtitle: 'Il primo fumetto di Moebius che mi capitò fra le mani, all\'epoca non sapevo nemmeno si chiamasse Jean Giraud, non è stato qualcosa di fondamentale come Arzach o essenziale come la saga dell\'Incal, ma qualcosa di commerciale, un\'opera minore, se così la possiamo definire, come la sua interpretazione di Silver Surfer, all\'epoca un personaggio secondario della Marvel ma già ricco di fascino cosmico, in una graphic novel (Parabola) scritta dallo stesso Stan Lee.',
       media: [
         {
           type: 'photo',
-          src: '/images/surf1.jpg',
+          src: '/images/moebius2.jpg',
         },
         {
           type: 'photo',
-          src: '/images/surf2.jpg',
+          src: '/images/moebius3.jpg',
         },
         {
           type: 'photo',
-          src: '/images/surf3.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf4.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf5.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf6.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf7.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf8.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf9.jpg',
+          src: '/images/moebius1.jpg',
         },
       ],
     },
   },
   {
     heading: {
-      title: 'Femminicidio, via libera dalla Camera a legge per tutelare gli orfani',
-      subtitle: 'Rischia l\'ergastolo chi uccide la coniuge o la convivente. E ai figli delle vittime viene assicurato patrocinio legale e reversibilità della pensione',
+      title: 'Giro completo della Corsica in bicicletta',
+      subtitle: 'Giro completo della Corsica, partendo da BASTIA (nave da Livorno, le bici non pagano, circa 4 ore), in senso antiorario: circa 800 chilometri. Sconsigliabile in senso orario, quasi sempre vento forte favorevole alla marcia in senso antiorario. Sconsigliabile dai primi di giugno fino ai primi di settembre: il sole BRUCIA VERAMENTE, e gran caldo.',
       media: [
         {
           type: 'photo',
-          src: '/images/surf1.jpg',
+          src: '/images/corsica1.jpg',
         },
         {
           type: 'photo',
-          src: '/images/surf2.jpg',
+          src: '/images/corsica2.jpg',
         },
         {
           type: 'photo',
-          src: '/images/surf3.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf4.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf5.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf6.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf7.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf8.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf9.jpg',
-        },
-      ],
-    },
-  },
-  {
-    heading: {
-      title: 'Femminicidio, via libera dalla Camera a legge per tutelare gli orfani',
-      subtitle: 'Rischia l\'ergastolo chi uccide la coniuge o la convivente. E ai figli delle vittime viene assicurato patrocinio legale e reversibilità della pensione',
-      media: [
-        {
-          type: 'photo',
-          src: '/images/surf1.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf2.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf3.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf4.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf5.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf6.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf7.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf8.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf9.jpg',
-        },
-      ],
-    },
-  },
-  {
-    heading: {
-      title: 'Femminicidio, via libera dalla Camera a legge per tutelare gli orfani',
-      subtitle: 'Rischia l\'ergastolo chi uccide la coniuge o la convivente. E ai figli delle vittime viene assicurato patrocinio legale e reversibilità della pensione',
-      media: [
-        {
-          type: 'photo',
-          src: '/images/surf1.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf2.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf3.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf4.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf5.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf6.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf7.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf8.jpg',
-        },
-        {
-          type: 'photo',
-          src: '/images/surf9.jpg',
+          src: '/images/corsica3.jpg',
         },
       ],
     },
