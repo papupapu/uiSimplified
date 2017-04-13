3ab4931f2eeae0e21d9d6666dbb3560c3cb142ad
Giancarlo Gerbaz <giancarlo.gerbaz@casa.it>
2017-04-13 14:49:22 +0200

    stackoverflow

--- src/browser/routes.jsx
+++ src/browser/routes.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import UiSimplified from './UiSimplified';
 
 // http://stackoverflow.com/questions/33996484/using-multiple-layouts-for-react-router-components
+// http://stackoverflow.com/questions/43333920/multiple-layouts-with-react-router-v4?noredirect=1&lq=1
+// https://reacttraining.com/react-router/native/example/route-config
 
 const UiRouter = () => (
   <Router>
